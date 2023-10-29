9664c9a2d69013b448a157abef24b28a01aa980d
YuriyDub <yuriyyuriovich@gmail.com>
2023-10-29 13:15:05 +0200

    upgrade course page

--- src/pages/CoursePage/CoursePage.jsx
+++ src/pages/CoursePage/CoursePage.jsx
@@ -21,13 +21,31 @@ export const CoursePage = () => {
         ) : (
           <>
             <div className={styles.course}>
-              <section className={styles.description}>
+              <section className={styles.details}>
                 <h1 className={styles.title}>{data.title}</h1>
-                <p className={styles.details}>{data.description}</p>
+                <p className={styles.description}>{data.description}</p>
+                <div className={styles.chips}>
+                  <span className={styles.chip}>{data.category}</span>
+                  <span className={styles.chip}>{data.duration} duration</span>
+                  <span className={styles.chip}>{data.level} level</span>
+                  <span className={styles.chip}>{data.language}</span>
+                  <span className={styles.chip}>{data?.lessons?.length} lessons</span>
+                </div>
+                <ul className={styles.lessons}>
+                  {data?.lessons?.map((l) => (
+                    <li className={styles.lesson}>
+                      <div className={styles.description}>
+                        <h2>{l.title}</h2>
+                        <p>{l.description}</p>
+                      </div>
+                      <span className={styles.duration}>{l.duration}</span>
+                    </li>
+                  ))}
+                </ul>
+                <span className={styles.author}>{data.instructor.name}</span>
               </section>
               <section>
                 <img src={placeholder} alt="placeholder" className={styles.preview} />
-                <span className={styles.author}>{data.instructor.name}</span>
               </section>
             </div>
           </>

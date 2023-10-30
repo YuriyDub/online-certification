d66ce696d866e364933d3dec523e814b159140c8
YuriyDub <yuriyyuriovich@gmail.com>
2023-10-30 13:15:06 +0200

    add loaders

--- src/utils/network.js
+++ src/utils/network.js
@@ -16,7 +16,7 @@ instanceAxios.interceptors.response.use(
   (config) => config,
   async (error) => {
     const originalRequest = error.config;
-    if (error.response.status == 401 && !error.config._isRetry) {
+    if (error.response.status === 401 && !error.config._isRetry) {
       originalRequest._isRetry = true;
       try {
         const response = await axios.get(`${API_URL}/refresh`, { withCredentials: true });
